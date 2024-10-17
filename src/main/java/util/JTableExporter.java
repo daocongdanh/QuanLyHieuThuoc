19bcc477c026721588bff878d05adbecf497770a
Viet Hoang <gvhoang30112004@gmail.com>
2024-10-17 12:33:58 +0700

    fix Excel

--- src/main/java/util/JTableExporter.java
+++ src/main/java/util/JTableExporter.java
@@ -1,32 +1,29 @@
-package util;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.Desktop;
 import java.io.File;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.table.TableModel;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class JTableExporter {
-    
+
     public static void openFile(String file) throws IOException {
         File path = new File(file);
         Desktop.getDesktop().open(path);
     }
-    
+
     public static void exportJTableToExcel(JTable table) {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Chọn đường dẫn lưu file Excel");
         FileNameExtensionFilter filter = new FileNameExtensionFilter("XLSX files", "xlsx");
         fileChooser.setFileFilter(filter);
         fileChooser.setAcceptAllFileFilterUsed(false);
-        
+
         int userChoice = fileChooser.showSaveDialog(null);
         if (userChoice == JFileChooser.APPROVE_OPTION) {
             try {
@@ -34,12 +31,20 @@ public class JTableExporter {
                 if (!filePath.toLowerCase().endsWith(".xlsx")) {
                     filePath += ".xlsx";
                 }
-                
+
                 TableModel model = table.getModel();
-                Workbook workbook = new XSSFWorkbook();
+                Workbook workbook;
+
+                // Khởi tạo workbook cho định dạng xlsx
+                if (filePath.endsWith(".xlsx")) {
+                    workbook = new XSSFWorkbook();
+                } else {
+                    workbook = new HSSFWorkbook();
+                }
+
                 Sheet sheet = workbook.createSheet("Sheet1");
 
-                // Create header row
+                // Tạo dòng tiêu đề
                 CellStyle cellStyle = createStyleForHeader(sheet);
                 Row headerRow = sheet.createRow(0);
                 for (int i = 0; i < model.getColumnCount(); i++) {
@@ -48,7 +53,7 @@ public class JTableExporter {
                     headerCell.setCellStyle(cellStyle);
                 }
 
-                // Create data rows
+                // Tạo dòng dữ liệu
                 for (int i = 0; i < model.getRowCount(); i++) {
                     Row dataRow = sheet.createRow(i + 1);
                     for (int j = 0; j < model.getColumnCount(); j++) {
@@ -60,37 +65,35 @@ public class JTableExporter {
                     }
                 }
 
-                // Resize all columns to fit the content size
+                // Điều chỉnh kích thước cột
                 for (int i = 0; i < model.getColumnCount(); i++) {
                     sheet.autoSizeColumn(i);
                 }
 
-                // Write the output to a file
+                // Ghi dữ liệu ra file
                 try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
                     workbook.write(fileOut);
                 }
-                
-                workbook.close();
+
                 openFile(filePath);
             } catch (IOException ex) {
-                MessageDialog.error(null, "Lỗi đọc file!");
+                JOptionPane.showMessageDialog(null, "Lỗi đọc file!", "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
-    
+
     private static CellStyle createStyleForHeader(Sheet sheet) {
-        // Create font
+        // Tạo font chữ
         Font font = sheet.getWorkbook().createFont();
         font.setFontName("Times New Roman");
-        font.setBold(true);
-        font.setFontHeightInPoints((short) 14); // font size
-        font.setColor(IndexedColors.WHITE.getIndex()); // text color
+        font.setBoldweight(Font.BOLDWEIGHT_BOLD);  // BOLDWEIGHT cho Apache POI 3.9
+        font.setFontHeightInPoints((short) 14);
 
-        // Create CellStyle
+        // Tạo CellStyle
         CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
         cellStyle.setFont(font);
-        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        cellStyle.setBorderBottom(CellStyle.BORDER_THIN);
         return cellStyle;
     }
 }
